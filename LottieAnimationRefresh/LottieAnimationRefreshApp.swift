01635b505055b805d68e963ab10ba86a566537c4
abdallah.mohammed <abdallahabdulzahir@gmail.com>
2025-08-03 07:16:33 +0300

    Initial Commit

---
+++ LottieAnimationRefresh/LottieAnimationRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationRefreshApp.swift
+//  LottieAnimationRefresh
+//
+//  Created by name on 03/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
